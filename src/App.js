ce723aa90f0afba02eb1bff84e7aa1b79097dea6
akyp888 <akashrkashyap98@gmail.com>
2021-08-24 23:04:27 +0530

    added searchbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
     <NavBar />
+    <SearchBar />
     </div>
   );
 }
