742af149c1f40025413f4de995fe23edadaf3b84
akshay-eng <akshaysdeekshith@gmail.com>
2021-08-26 00:51:46 +0530

    added clg clgs clg photo

---
+++ src/components/College.js
@@ -0,0 +1,21 @@
+
+
+const College =()=>{
+
+
+
+    return(
+        <>
+        <div className="col-sm-4">
+ <div className="dedicated">
+                <div className="icon"><a href="#"><img src="images/icon-2.png" /></a></div>
+                <h2 className="hosting_text">Dedicated Hosting</h2>
+                <p className="justo_text">Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci.</p>
+              </div>
+              </div>
+
+        </>
+
+    )
+}
+export default College
