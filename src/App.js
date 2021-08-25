024fc658ff4f028d55a9998d2d1610f29bbb30b8
akshay-eng <akshaysdeekshith@gmail.com>
2021-08-25 23:11:07 +0530

    added college cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Cards from "./components/Cards";
+import Colleges from "./components/Colleges";
+import Stuff from "./components/Stuff";
 
 function App() {
   return(
     <>
 <Cards/>
+<Stuff/>
+<Colleges/>
 
 
     </>
