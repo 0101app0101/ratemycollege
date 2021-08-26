a37cb98f914af77d3dd4a9575da516474276ebe1
akshay-eng <akshaysdeekshith@gmail.com>
2021-08-26 23:05:57 +0530

    doin rstingng

--- src/components/Ratingpg.js
+++ src/components/Ratingpg.js
@@ -18,31 +18,7 @@ const Ratingpg=()=>{
 
 
 
-<div>
-<div className="container pb-cmnt-container">
-  <div className="row">
-    <div className="col-md-6 offset-md-3">
-      <div className="card card-info">
 
-        <div className="card-block">
-          <textarea placeholder="Write your comment here!" className="pb-cmnt-textarea" defaultValue={""} />
-          <form className="form-inline">
-
-            <div className="btn-group">
-              <button className="btn" type="button"><span className="fa fa-picture-o fa-lg" /></button>
-              <button className="btn" type="button"><span className="fa fa-video-camera fa-lg" /></button>
-              <button className="btn" type="button"><span className="fa fa-microphone fa-lg" /></button>
-              <button className="btn" type="button"><span className="fa fa-music fa-lg" /></button>
-            </div>
-            <button className="btn btn-primary float-xs-right" type="button">Share</button>
-          </form>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-<style dangerouslySetInnerHTML={{__html: "\n    .pb-cmnt-container {\n        font-family: Lato;\n        margin-top: 100px;\n    }\n\n    .pb-cmnt-textarea {\n        resize: none;\n        padding: 20px;\n        height: 130px;\n        width: 100%;\n        border: 1px solid #F2F2F2;\n    }\n" }} />
-</div>
 </>
 
 
