abafc246e6ec1e367244a8a7292f301d100a5eb8
akshay-eng <akshaysdeekshith@gmail.com>
2021-08-28 11:48:39 +0530

    replaced sliders with stars

--- src/components/Ratingpg.js
+++ src/components/Ratingpg.js
@@ -1,4 +1,5 @@
-import { Slider } from '@material-ui/core';
+
+import Stars from './Stars'
 
 
 
@@ -28,309 +29,59 @@ const Ratingpg = () => {
             <div className='rate__slider__field'>
               <span className='MuiSlider-root jss125 MuiSlider-colorPrimary MuiSlider-marked'>
                   
-              <Slider/>
+             <Stars/>
          
               
               </span>
               
             </div>
           </div>
+                  
           <div className='rate__slider'>
             <div className='rate__slider__question'>
               <div className='u-m-bottom--xs u-t--md'>
-                Rate the <span className='u-t--bold u-t--primary'>building</span>{" "}
-                out of 5 stars
+                Rate the <span className='u-t--bold u-t--primary'>room</span> out
+                of 5 stars
               </div>
               <div className='u-t--gray'>
-                Keep in mind: amenities, security, age of building
+                Keep in mind: size, comfort, natural lighting
               </div>
             </div>
             <div className='rate__slider__field'>
               <span className='MuiSlider-root jss125 MuiSlider-colorPrimary MuiSlider-marked'>
-                <span className='MuiSlider-rail' />
-                <span
-                  className='MuiSlider-track'
-                  style={{ left: "0%", width: "50%" }}
-                />
-                <input type='hidden' defaultValue={3} />
-                <span
-                  data-index={0}
-                  className='MuiSlider-mark MuiSlider-markActive'
-                  style={{ left: "0%" }}
-                />
-                <span
-                  aria-hidden='true'
-                  data-index={0}
-                  className='MuiSlider-markLabel MuiSlider-markLabelActive'
-                  style={{ left: "0%" }}
-                >
-                  1
-                </span>
-                <span
-                  data-index={1}
-                  className='MuiSlider-mark MuiSlider-markActive'
-                  style={{ left: "25%" }}
-                />
-                <span
-                  aria-hidden='true'
-                  data-index={1}
-                  className='MuiSlider-markLabel MuiSlider-markLabelActive'
-                  style={{ left: "25%" }}
-                >
-                  2
-                </span>
-                <span
-                  data-index={2}
-                  className='MuiSlider-mark MuiSlider-markActive'
-                  style={{ left: "50%" }}
-                />
-                <span
-                  aria-hidden='true'
-                  data-index={2}
-                  className='MuiSlider-markLabel MuiSlider-markLabelActive'
-                  style={{ left: "50%" }}
-                >
-                  3
-                </span>
-                <span
-                  data-index={3}
-                  className='MuiSlider-mark'
-                  style={{ left: "75%" }}
-                />
-                <span
-                  aria-hidden='true'
-                  data-index={3}
-                  className='MuiSlider-markLabel'
-                  style={{ left: "75%" }}
-                >
-                  4
-                </span>
-                <span
-                  data-index={4}
-                  className='MuiSlider-mark'
-                  style={{ left: "100%" }}
-                />
-                <span
-                  aria-hidden='true'
-                  data-index={4}
-                  className='MuiSlider-markLabel'
-                  style={{ left: "100%" }}
-                >
-                  5
-                </span>
-                <span
-                  className='MuiSlider-thumb MuiSlider-thumbColorPrimary'
-                  tabIndex={0}
-                  role='slider'
-                  data-index={0}
-                  aria-orientation='horizontal'
-                  aria-valuemax={5}
-                  aria-valuemin={1}
-                  aria-valuenow={3}
-                  style={{ left: "50%" }}
-                />
+                  
+             <Stars/>
+         
+              
               </span>
+              
             </div>
           </div>
+                  
           <div className='rate__slider'>
             <div className='rate__slider__question'>
               <div className='u-m-bottom--xs u-t--md'>
-                Rate the <span className='u-t--bold u-t--primary'>bathroom</span>{" "}
-                out of 5 stars
+                Rate the <span className='u-t--bold u-t--primary'>room</span> out
+                of 5 stars
               </div>
               <div className='u-t--gray'>
-                Keep in mind: cleanliness, private vs communal
+                Keep in mind: size, comfort, natural lighting
               </div>
             </div>
             <div className='rate__slider__field'>
               <span className='MuiSlider-root jss125 MuiSlider-colorPrimary MuiSlider-marked'>
-                <span className='MuiSlider-rail' />
-                <span
-                  className='MuiSlider-track'
-                  style={{ left: "0%", width: "50%" }}
-                />
-                <input type='hidden' defaultValue={3} />
-                <span
-                  data-index={0}
-                  className='MuiSlider-mark MuiSlider-markActive'
-                  style={{ left: "0%" }}
-                />
-                <span
-                  aria-hidden='true'
-                  data-index={0}
-                  className='MuiSlider-markLabel MuiSlider-markLabelActive'
-                  style={{ left: "0%" }}
-                >
-                  1
-                </span>
-                <span
-                  data-index={1}
-                  className='MuiSlider-mark MuiSlider-markActive'
-                  style={{ left: "25%" }}
-                />
-                <span
-                  aria-hidden='true'
-                  data-index={1}
-                  className='MuiSlider-markLabel MuiSlider-markLabelActive'
-                  style={{ left: "25%" }}
-                >
-                  2
-                </span>
-                <span
-                  data-index={2}
-                  className='MuiSlider-mark MuiSlider-markActive'
-                  style={{ left: "50%" }}
-                />
-                <span
-                  aria-hidden='true'
-                  data-index={2}
-                  className='MuiSlider-markLabel MuiSlider-markLabelActive'
-                  style={{ left: "50%" }}
-                >
-                  3
-                </span>
-                <span
-                  data-index={3}
-                  className='MuiSlider-mark'
-                  style={{ left: "75%" }}
-                />
-                <span
-                  aria-hidden='true'
-                  data-index={3}
-                  className='MuiSlider-markLabel'
-                  style={{ left: "75%" }}
-                >
-                  4
-                </span>
-                <span
-                  data-index={4}
-                  className='MuiSlider-mark'
-                  style={{ left: "100%" }}
-                />
-                <span
-                  aria-hidden='true'
-                  data-index={4}
-                  className='MuiSlider-markLabel'
-                  style={{ left: "100%" }}
-                >
-                  5
-                </span>
-                <span
-                  className='MuiSlider-thumb MuiSlider-thumbColorPrimary'
-                  tabIndex={0}
-                  role='slider'
-                  data-index={0}
-                  aria-orientation='horizontal'
-                  aria-valuemax={5}
-                  aria-valuemin={1}
-                  aria-valuenow={3}
-                  style={{ left: "50%" }}
-                />
+                  
+             <Stars/>
+         
+              
               </span>
-            </div>
-          </div>
-          <div className='rate__slider'>
-            <div className='rate__slider__question'>
-              <div className='u-m-bottom--xs u-t--md'>
-                Rate the <span className='u-t--bold u-t--primary'>location</span>{" "}
-                out of 5 stars
-              </div>
-              <div className='u-t--gray'>
-                Keep in mind: distance to classes, safety of area
-              </div>
-            </div>
-            <div className='rate__slider__field'>
-              <span className='MuiSlider-root jss125 MuiSlider-colorPrimary MuiSlider-marked'>
-                <span className='MuiSlider-rail' />
-                <span
-                  className='MuiSlider-track'
-                  style={{ left: "0%", width: "50%" }}
-                />
-                <input type='hidden' defaultValue={3} />
-                <span
-                  data-index={0}
-                  className='MuiSlider-mark MuiSlider-markActive'
-                  style={{ left: "0%" }}
-                />
-                <span
-                  aria-hidden='true'
-                  data-index={0}
-                  className='MuiSlider-markLabel MuiSlider-markLabelActive'
-                  style={{ left: "0%" }}
-                >
-                  1
-                </span>
-                <span
-                  data-index={1}
-                  className='MuiSlider-mark MuiSlider-markActive'
-                  style={{ left: "25%" }}
-                />
-                <span
-                  aria-hidden='true'
-                  data-index={1}
-                  className='MuiSlider-markLabel MuiSlider-markLabelActive'
-                  style={{ left: "25%" }}
-                >
-                  2
-                </span>
-                <span
-                  data-index={2}
-                  className='MuiSlider-mark MuiSlider-markActive'
-                  style={{ left: "50%" }}
-                />
-                <span
-                  aria-hidden='true'
-                  data-index={2}
-                  className='MuiSlider-markLabel MuiSlider-markLabelActive'
-                  style={{ left: "50%" }}
-                >
-                  3
-                </span>
-                <span
-                  data-index={3}
-                  className='MuiSlider-mark'
-                  style={{ left: "75%" }}
-                />
-                <span
-                  aria-hidden='true'
-                  data-index={3}
-                  className='MuiSlider-markLabel'
-                  style={{ left: "75%" }}
-                >
-                  4
-                </span>
-                <span
-                  data-index={4}
-                  className='MuiSlider-mark'
-                  style={{ left: "100%" }}
-                />
-                <span
-                  aria-hidden='true'
-                  data-index={4}
-                  className='MuiSlider-markLabel'
-                  style={{ left: "100%" }}
-                >
-                  5
-                </span>
-                <span
-                  className='MuiSlider-thumb MuiSlider-thumbColorPrimary'
-                  tabIndex={0}
-                  role='slider'
-                  data-index={0}
-                  aria-orientation='horizontal'
-                  aria-valuemax={5}
-                  aria-valuemin={1}
-                  aria-valuenow={3}
-                  style={{ left: "50%" }}
-                />
-              </span>
+              
             </div>
           </div>
           <div class="form-group">
   <label for="comment">Comment:</label>
   <textarea class="form-control" rows="5" id="comment"></textarea>
 </div>
-          
         </form>
       </>
     );
