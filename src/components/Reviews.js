03cfdef498db761d4b391802b4a4fe5fed04d0c2
akyp888 <akashrkashyap98@gmail.com>
2021-09-11 15:56:51 +0530

    added modal

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,8 +1,13 @@
-
-
+import { useState } from "react"
 import ReviewCard from "./ReviewCard"
-
+import ReactModal from "react-modal"
+import CollegeReviewForm from "./CollegeReviewForm"
 const Reviews =()=>{
+
+  const [showModal,setShowModal] = useState(false) 
+
+  const handleOpenModal = () => setShowModal(true)
+  const handleCloseModal = () => setShowModal(false)
 
     return(
       <>
@@ -12,10 +17,10 @@ const Reviews =()=>{
           <div className="row d-flex justify-content-center">
             <div className="col-md-12 col-lg-10 col-xl-8">
               <div className="d-flex justify-content-between align-items-center mb-4">
-                <h4 className="text-dark mb-0">Unread comments (4)</h4>
+                <h4 className="text-dark mb-0">Comments (4)</h4>
                 <div className="card">
                   <div className="">
-                   <button className="btn btn-primary">Add Review</button>
+                   <button className="btn btn-primary" onClick={handleOpenModal}  >Add Review</button>
                   </div>
                 </div>
               </div>
@@ -27,6 +32,12 @@ const Reviews =()=>{
               </div>
           </div>
         </div>
+
+        <ReactModal isOpen={showModal} onRequestClose={handleCloseModal} >
+          <CollegeReviewForm />
+          <button className="btn btn-danger" onClick={handleCloseModal}>Close</button>
+        </ReactModal>
+
       </section>
       </>
     )
